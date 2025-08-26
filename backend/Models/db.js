de99dec2b5f5621ced50f5fbaaf2ce8a5ba33a55
DivCodexweb <divcodexweb@gmail.com>
2025-08-26 16:03:30 +0500

    push

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -16,13 +16,13 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 // Models import karo
-db.User = require("./User")(sequelize, Sequelize);
-db.Employee = require("./Employee")(sequelize, Sequelize);
-db.Clients = require("./Clients")(sequelize, Sequelize);
-db.Income = require("./Income")(sequelize, Sequelize);
-db.Expancetype = require("./Expancetype")(sequelize, Sequelize);
-db.Expance = require("./Expance")(sequelize, Sequelize);
-db.Salery = require("./Salery")(sequelize, Sequelize);
+db.User = require("./User.js")(sequelize, Sequelize);
+db.Employee = require("./Employee.js")(sequelize, Sequelize);
+db.Clients = require("./Clients.js")(sequelize, Sequelize);
+db.Income = require("./Income.js")(sequelize, Sequelize);
+db.Expancetype = require("./Expancetype.js")(sequelize, Sequelize);
+db.Expance = require("./Expance.js")(sequelize, Sequelize);
+db.Salery = require("./Salery.js")(sequelize, Sequelize);
 
 
 module.exports = db;
